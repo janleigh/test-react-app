c1ee85eab623512fffe37ce3b97a9a5de643e454
Jan Leigh Muñoz <janleigh@proton.me>
2025-08-20 20:46:19 +0800

    refactor: project structure and update dependencies
    - Changed project name from "test2" to "test" in package.json and package-lock.json.
    - Added react-router-dom as a dependency.
    - Implemented routing in App.js with Home, Inbox, Login, and Register components.
    - Created new pages: Home, Inbox, Login, and Register.
    - Removed unused files: App.test.js, reportWebVitals.js, setupTests.js, and logo.svg.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        // <div style={{
+        //     alignItems: 'center',
+        //     display: 'flex',
+        //     flexDirection: 'column',
+        //     height: '100vh',
+        //     justifyContent: 'center',
+        //     color: '#fff',
+        //     backgroundColor: '#171717'
+        // }}>
+        <div>
+            <h1>
+                Is Dev <a href="/" style={{
+                    color: 'pink'
+                }}>Gay</a>?
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
